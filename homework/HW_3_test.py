851d67be8170d7462fa557caca485cca35a63dc4
IVAN IVANSKYI <meternih@gmail.com>
2021-01-21 17:34:34 +0200

    done test_simple_calc.py

--- homework/HW_3_test.py
+++ homework/HW_3_test.py
@@ -9,21 +9,24 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(add(5, -6), -1)
         self.assertEqual(add(1.2, 3.4), 4.6)
 
-    def test_substract(self):
+    def test_subtract(self):
         self.assertEqual(subtract(6, 6), 0)
         self.assertEqual(subtract(-6, 6), 0)
         self.assertEqual(subtract(6.6, 6.6), 13.2)
 
-
     def test_multiply(self):
         self.assertEqual(multiply(10, 10), 100)
         self.assertEqual(multiply(10, -10), -100)
-        self.assertEqual(multiply(10.5, 10.5), 107.1)
+        self.assertEqual(multiply(10.5, 10.5), 110.25)
 
     def test_divide(self):
         self.assertEqual(divide(15, 15), 1)
         self.assertEqual(divide(-15, 15), -1)
         self.assertEqual(divide(15, 6), 2.5)
+
+    def test_divide_not_zero(self):
+        with self.assertRaises(ValueError):
+            divide(6, 0)
 
 
 if __name__ == '__main__':
