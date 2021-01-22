cc3f1d25340fbd7ab92684a7fa7f4bbd09e57102
IVAN IVANSKYI <meternih@gmail.com>
2021-01-22 17:11:49 +0200

    done test_simple_calc

---
+++ homework/HW_3_test.py
@@ -0,0 +1,34 @@
+import unittest
+from homework.test_simple_calc import add, subtract, multiply, divide
+
+
+class TestCalc(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(2, 6), 8)
+        self.assertEqual(add(5, -6), -1)
+        self.assertEqual(add(1.2, 3.4), 4.6)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(6, 6), 0)
+        self.assertEqual(subtract(-6, 6), 0)
+        self.assertEqual(subtract(6.6, 6.6), 13.2)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(10, 10), 100)
+        self.assertEqual(multiply(10, -10), -100)
+        self.assertEqual(multiply(10.5, 10.5), 110.25)
+
+    def test_divide(self):
+        self.assertEqual(divide(15, 15), 1)
+        self.assertEqual(divide(-15, 15), -1)
+        self.assertEqual(divide(15, 6), 2.5)
+
+    def test_divide_not_zero(self):
+        with self.assertRaises(ValueError):
+            divide(6, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
+   
