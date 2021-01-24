60da0f470b75043ccc67e27cb08a22dbc24b5cd3
IVAN IVANSKYI <meternih@gmail.com>
2021-01-24 08:51:05 +0200

    add HW_3_test1.py

--- homework/HW_3_test1.py
+++ homework/HW_3_test1.py
@@ -1,9 +1,11 @@
 import unittest
 from homework.tests_simple_employee import Employee
+from unittest.mock import patch
 
 
 class TestSimpleEmployee(unittest.TestCase):
 
+    @classmethod
     def setUp(self) -> None:
         self.employee = Employee("Thor", "Odinson", 2000)
 
@@ -15,10 +17,12 @@ class TestSimpleEmployee(unittest.TestCase):
 
     def test_apply_raise(self):
         self.employee.apply_raise()
-        self.assertEqual(self.employee.pay, 2000)
+        self.assertEqual(self.employee.pay, 2500)
 
-    # def test_monthly_schedule(self):
-    #     pass
+    def test_monthly_schedule(self):
+        with patch('tests_simple_employee.requests.get') as mock_requests:
+            mock_requests.return_value.ok = True
+        self.assertNotEqual(self.employee.monthly_schedule('june'), 'Bad Response!')
 
 
 if __name__ == "__main__":
